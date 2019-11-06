187616bca8b597d48f4561da5b6d89f833dc541a
kyle1668 <kyledevinobrien1@gmail.com>
2019-11-06 14:42:49 -0800

    added support for checking api key in env var

--- RequestTrigger/index.js
+++ RequestTrigger/index.js
@@ -1,16 +1,44 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const authenticateApiKey = (functionContext, requestObject) => {
+  const headers = requestObject.headers;
+  const requestKey = headers.authentication;
+  const correctKey = process.env["API_KEY"];
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+  functionContext.log.warn(JSON.stringify(headers))
+  functionContext.log.warn(requestKey)
+  functionContext.log.warn(correctKey)
+
+  if (correctKey === undefined) {
+    functionContext.log.error("ERROR: UNSET API KEY ENV VAR");
+    return false;
+  }
+
+  return requestKey === correctKey;
+};
+
+// {
+//     "method": "GET",
+//     "url": "http://localhost:7071/api/RequestTrigger",
+//     "originalUrl": "http://localhost:7071/api/RequestTrigger",
+//     "headers": {
+//       "accept": "*/*",
+//       "host": "localhost:7071",
+//       "user-agent": "insomnia/7.0.3"
+//     },
+//     "query": {},
+//     "params": {}
+//   }
+
+module.exports = async function(context, req) {
+  const isAuthenticated = authenticateApiKey(context, req);
+
+  if (isAuthenticated === false) {
+    context.res = {
+      status: 401,
+      body: {
+        error: true,
+        code: 400,
+        errorMessage: "Unable to authenticate request."
+      }
+    };
+  }
 };
