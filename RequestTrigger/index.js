6ace5851ad117348dcd82ceb393cd3a11f7e3a0c
Isai Lopez <ilopezro@ucsc.edu>
2019-11-06 14:51:38 -0800

    set up post request that inserts into db

--- RequestTrigger/index.js
+++ RequestTrigger/index.js
@@ -14,8 +14,7 @@ const db = require('knex')({
 
 const recordExist = async (req) => {
     // Since req.query.type is singular, we add an s to make it plural (and to conform to the table names)
-    console.log(req.body)
-    console.log("body of request here")
+
     const doesExist = await db(`${req.body.type}s`).where('email', req.body.email).then((exist) => {
         if (exist.length === 0) return false
         else return true
@@ -26,32 +25,93 @@ const recordExist = async (req) => {
     return doesExist
 }
 
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const insert = async (req) => {
 
-    if(req.method == "GET"){
-        console.log(req)
+    let application = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email
+    }
+
+    // build fields based on application type
+    // if (req.query.type == "hacker" || req.query.type == "mentor" || req.query.type == "volunteer") {
+    //     application.age = req.body.age
+    //     application.gender = req.body.gender
+    //     application.ethnicity = req.body.ethnicity
+    //     application.firstCruzHacks = req.body.firstCruzHacks
+    //     application.whyParticipate = req.body.whyParticipate
+    //     application.whatSee = req.body.whatSee
+    //     application.dietaryRestrictions = req.body.dietaryRestrictions
+    //     application.restPlace = req.body.restPlace
+    //     application.parking = req.body.parking
+    //     application.accomodations = req.body.accomodations
+
+    //     if (req.query.type == "mentor") {
+    //         application.schoolOrCompany = req.body.schoolOrCompany
+    //     }
+    //     if (req.query.type == "hacker") {
+    //         application.ucscStudent = req.body.ucscStudent
+    //         application.ucscCollege = req.body.ucscCollege
+    //         application.school = req.body.school
+    //         application.techGoals = req.body.techGoals
+    //         application.haveTeam = req.body.haveTeam
+    //         application.teamNames = req.body.teamNames
+    //         application.teamEmails = req.body.teamEmails
+    //     }
+    //     if (req.query.type == "volunteer") {
+    //         application.cruzID = req.body.cruzID
+    //     }
+    //     if (req.query.type != "volunteer") {
+    //         application.linkedin = req.body.linkedin
+    //         application.github = req.body.github
+    //         application.transport = req.body.transport
+    //     }
+    //     if (req.query.type != "mentor") {
+    //         application.major = req.body.major
+    //         application.gradYear = req.body.gradYear
+    //         application.firstHackathon = req.body.firstHackathon
+    //     }
+    // }
+    await db(`${req.body.type}s`).insert(application).then(() => {
+        console.log(`${req.body.email} has been added to table ${req.body.type}s`)
+    }).catch((err) => {
+        console.log(err);
+        throw err
+    })
+}
+
+module.exports = async function (context, req) {
+    // if(req.method == "GET"){
+    //     const exist = await recordExist(req)
+    //     if(exist){
+    //         context.res = {
+    //             body: "you connected with the db"
+    //         }
+    //     }else{
+    //         context.res = {
+    //             body: "you kinda failed"
+    //         }
+    //     }
+    // }
+
+    if(req.method == "POST"){
         const exist = await recordExist(req)
+
         if(exist){
             context.res = {
-                body: "you connected with the db"
+                body: "Person already exists in database"
             }
         }else{
-            context.res = {
-                body: "you kinda failed"
+            try {
+                await insert(req)
+                context.res = {
+                    body: `Successfully saved ${req.body.email} to database!`
+                }
+            } catch (err) {
+                context.res = {
+                    body: `${JSON.stringify(err.stack)}`
+                }
             }
         }
     }
-    // if (req.query.name || (req.body && req.body.name)) {
-    //     context.res = {
-    //         // status: 200, /* Defaults to 200 */
-    //         body: "Hello " + (req.query.name || req.body.name)
-    //     };
-    // }
-    // else {
-    //     context.res = {
-    //         status: 400,
-    //         body: "Please pass a name on the query string or in the request body"
-    //     };
-    // }
 };
