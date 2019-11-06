5a3c518fe32adacca5b811b43dfad4316058a3b0
kyle1668 <kyledevinobrien1@gmail.com>
2019-11-06 15:51:36 -0800

    added dummy hacker response for success get request

--- RequestTrigger/index.js
+++ RequestTrigger/index.js
@@ -1,4 +1,4 @@
-const authenticateApiKey = require("./middleware/authentication")
+const authenticateApiKey = require("./middleware/authentication");
 
 module.exports = async function(context, req) {
   const isAuthenticated = authenticateApiKey(context, req);
@@ -8,9 +8,38 @@ module.exports = async function(context, req) {
       status: 401,
       body: {
         error: true,
-        code: 400,
+        status: 401,
         errorMessage: "Unable to authenticate request."
       }
     };
+    context.done();
   }
+
+  if (req.method === "GET") {
+    context.res = {
+      status: 200,
+      body: {
+        error: false,
+        status: 200,
+        message: "hacker data successfully",
+        count: 1,
+        results: [
+          {
+            firstName: "Hank",
+            lastName: "Turley",
+            email: "HankT@ucsc.edu",
+            age: 33,
+            school: "University of Califonia, Santa Cruz",
+            major: "Turkey Sutdies",
+            github: "https://github.com/hank",
+            transport: false,
+            parking: false,
+            checkedIn: false
+          }
+        ]
+      }
+    };
+  }
+
+  context.log.info(JSON.stringify(req, null, 2));
 };
