ddc9c464dc40e4a7e392ddfcdcf51ca05cf08439
Isai Lopez <ilopezro@ucsc.edu>
2019-11-06 14:35:31 -0800

    made and tested connection to database

--- RequestTrigger/index.js
+++ RequestTrigger/index.js
@@ -1,16 +1,57 @@
+"use strict"
+require('dotenv').config()
+
+const db = require('knex')({
+    client: 'pg',
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_USER,
+        port: process.env.DB_PORT
+    }
+})
+
+const recordExist = async (req) => {
+    // Since req.query.type is singular, we add an s to make it plural (and to conform to the table names)
+    console.log(req.body)
+    console.log("body of request here")
+    const doesExist = await db(`${req.body.type}s`).where('email', req.body.email).then((exist) => {
+        if (exist.length === 0) return false
+        else return true
+    }).catch((err => {
+        console.log(err);
+        throw err
+    }));
+    return doesExist
+}
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+    if(req.method == "GET"){
+        console.log(req)
+        const exist = await recordExist(req)
+        if(exist){
+            context.res = {
+                body: "you connected with the db"
+            }
+        }else{
+            context.res = {
+                body: "you kinda failed"
+            }
+        }
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    // if (req.query.name || (req.body && req.body.name)) {
+    //     context.res = {
+    //         // status: 200, /* Defaults to 200 */
+    //         body: "Hello " + (req.query.name || req.body.name)
+    //     };
+    // }
+    // else {
+    //     context.res = {
+    //         status: 400,
+    //         body: "Please pass a name on the query string or in the request body"
+    //     };
+    // }
 };
