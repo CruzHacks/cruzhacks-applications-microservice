cc8fe5208bc705e8cb0d942ab5e6269b06281478
kyle1668 <kyledevinobrien1@gmail.com>
2019-11-10 16:36:14 -0800

    Added method to look up to see if an Auth- user exists by email

---
+++ RequestTrigger/middleware/account.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+
+const validateAuth0Email = email => {
+  const apiEndpoint = "https://ccruzhacks-2020.auth0.com/api/v2/users-by-email";
+  const requestToken = process.env.AUTH0_MANEGEMENT_API_TOKEN;
+  const requestConfig = {
+    headers: {
+      Authorization: `Bearer ${requestToken}`
+    },
+    params: {
+      email: email
+    }
+  };
+
+  return axios
+    .get(apiEndpoint, requestConfig)
+    .then(response => {
+      return response.data.length !== 0;
+    })
+    .catch(error => {
+      console.error(error);
+      return Promise.reject(error.message);
+    });
+};
+
+module.exports.validateAuth0Email = validateAuth0Email;
