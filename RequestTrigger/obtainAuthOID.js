fbf7356b2b60933ead19f1147004a7a8cf0c7129
Isai Lopez <ilopezro@ucsc.edu>
2019-11-15 15:32:11 -0800

    added logic if query was different than header

--- RequestTrigger/obtainAuthOID.js
+++ RequestTrigger/obtainAuthOID.js
@@ -1,7 +1,11 @@
 const getAuth0IdFromRequest = requestObject => {
     const headers = requestObject.headers;
     const queryParameters = requestObject.query;
-    return headers.auth0Id || queryParameters.accountId;
+    if(headers.auth0Id != queryParameters.accountId){
+        return undefined; 
+    }else{
+        return headers.auth0Id || queryParameters.accountId;
+    }
 };
 
 module.exports.getAuth0IdFromRequest = getAuth0IdFromRequest
