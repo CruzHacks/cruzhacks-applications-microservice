dadd53373d56bce29759b57e774de3aaf5c6472f
kyle1668 <kyledevinobrien1@gmail.com>
2019-11-19 12:25:19 -0800

    fixed syntax erro in catch statement

--- RequestTrigger/index.js
+++ RequestTrigger/index.js
@@ -1,7 +1,6 @@
 const authenticateApiKey = require("./middleware/authentication");
-const { getAuth0IdFromRequest } = require('./obtainAuthOID')
-const { getAccountData } = require('./getAccountData')
-
+const { getAuth0IdFromRequest } = require("./obtainAuthOID");
+const { getAccountData } = require("./getAccountData");
 
 module.exports = async function(context, req) {
   const isAuthenticated = authenticateApiKey(context, req);
@@ -16,8 +15,8 @@ module.exports = async function(context, req) {
         message: "Unable to authenticate request."
       }
     };
-    context.done()
-  }else if (auth0Id === undefined) {
+    context.done();
+  } else if (auth0Id === undefined) {
     context.res = {
       status: 400,
       body: {
@@ -33,21 +32,23 @@ module.exports = async function(context, req) {
     try {
       context.log("------------------------------------------------");
       const data = await getAccountData(context, req);
-      var bodyOfResponse; 
-      var statusCode; 
-      if(data.length === 0){
-        bodyOfResponse = `Could not obtain information on person with accountID ${req.query.accountId}`
-        statusCode = 404
-      }else{
-        bodyOfResponse = `Succesfully obtained information of ${auth0Id}\n\n` + JSON.stringify(data)
-        statusCode = 200
+      var bodyOfResponse;
+      var statusCode;
+      if (data.length === 0) {
+        bodyOfResponse = `Could not obtain information on person with accountID ${req.query.accountId}`;
+        statusCode = 404;
+      } else {
+        bodyOfResponse =
+          `Succesfully obtained information of ${auth0Id}\n\n` +
+          JSON.stringify(data);
+        statusCode = 200;
       }
 
       context.res = {
-        body: bodyOfResponse, 
+        body: bodyOfResponse,
         status: statusCode
-      }
-    } catch {
+      };
+    } catch(error) {
       context.res = {
         body: `Unable to obtain information for authoid ${req.query.accountId}`,
         status: 400
