1ad4bf5a4a285f7ba45d395e212c9d52016db0ae
kyle1668 <kyledevinobrien1@gmail.com>
2019-11-20 13:01:08 -0800

    added eslitn configs

--- RequestTrigger/obtainAuthOID.js
+++ RequestTrigger/obtainAuthOID.js
@@ -1,11 +1,11 @@
 const getAuth0IdFromRequest = requestObject => {
-    const headers = requestObject.headers;
-    const queryParameters = requestObject.query;
-    if(headers.auth0id != queryParameters.accountId){
-        return undefined; 
-    }else{
-        return headers.auth0id || queryParameters.accountId;
-    }
+  const headers = requestObject.headers;
+  const queryParameters = requestObject.query;
+  if (headers.auth0id != queryParameters.accountId) {
+    return undefined;
+  } 
+    return headers.auth0id || queryParameters.accountId;
+  
 };
 
-module.exports.getAuth0IdFromRequest = getAuth0IdFromRequest
+module.exports.getAuth0IdFromRequest = getAuth0IdFromRequest;
