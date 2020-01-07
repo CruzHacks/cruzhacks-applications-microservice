2484140d27d2b5117dd79f6f2ff1e5e4d6541b2f
kyle1668 <kyledevinobrien1@gmail.com>
2020-01-07 10:49:28 -0800

    changed the mocked resolve value for the databse module

--- RequestTrigger/__mocks__/database.js
+++ RequestTrigger/__mocks__/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   getUserDataByEmail: jest.fn(() => Promise.resolve({})),
   insertHackerApplication: jest.fn(() => Promise.resolve({})),
-  getAcceptanceStatus: jest.fn(() => Promise.resolve({})),
+  getAcceptanceStatus: jest.fn(() => Promise.resolve(true)),
 };
