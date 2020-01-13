882611e6c401f17735a3b1568ed5af263f2dd80e
mocklee <emockler@ucsc.edu>
2020-01-12 21:03:50 -0800

    set docker DB to default user

--- database/init.sql
+++ database/init.sql
@@ -166,6 +166,15 @@ CREATE TABLE volunteers
 )
 WITH (oids = false);
 
+
+DROP TABLE IF EXISTS announcements;
+CREATE TABLE announcements
+(
+    announceDatetime character varying(100) NOT NULL UNIQUE,
+    announcement character varying(160) NOT NULL,
+)
+WITH (oids = false);    
+
 -- -- insert mock data for hackers here
 -- insert into hackers
 --     (firstName, lastName, email, authOID, age, gender, ethnicity, school, gradYear, ucscStudent, ucscCollege, major, linkedin, github, firstHackathon, firstCruzHacks, whyParticipate, techGoals, whatSee, haveTeam, teamNames, teamEmails, dietaryRestrictions, transport, restPlace, parking, accomodations, confirmedUser, checkedIn)
@@ -740,5 +749,33 @@ WITH (oids = false);
 --     (firstname, lastname, authOid, email, team, checkedin)
 -- values
 --     ('Kalil', 'Schimek', '4dFJcM7', 'kschimekl@cdbaby.com', 'Logistics', false);
-
+-- -- mock announcement data
+-- insert into announcements
+--     (datetime, announcement)
+-- values 
+--      ('2019-02-03T14:31:55Z', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.');
+-- insert into announcements 
+--     (datetime, announcement) 
+--  values
+--      ('2020-01-07T23:00:58Z', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.');
+-- insert into announcements 
+--     (datetime, announcement) 
+-- values 
+--     ('2019-12-25T13:23:26Z', 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.');
+-- insert into announcements 
+--     (datetime, announcement) 
+-- values 
+--     ('2019-06-05T17:31:36Z', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.');
+--  insert into announcements 
+--      (datetime, announcement) 
+--  values 
+--      ('2019-09-28T17:51:34Z', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.');
+--  insert into announcements 
+--      (datetime, announcement) 
+--  values 
+--      ('2019-05-17T18:55:15Z', 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.');
+--  insert into announcements
+--      (datetime, announcement) 
+--  values 
+--      ('2019-01-16T23:40:13Z', 'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.');
 -- -- 2019-07-21 05:59:40.711244+00
